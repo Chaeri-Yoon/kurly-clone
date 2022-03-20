9b7638d336929f62919d3a6778b98cbb2e9c61dc
Chaeri <cryoon0103@gmail.com>
2022-03-20 23:46:54 +0000

    Create Sale Promo Banners. * It's currently possible to make it shift through a prev and a next button. And it will be made move automatically in the next commit.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,15 @@ import '../styles/reset.css';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import { SWRConfig } from 'swr';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SWRConfig value={{ fetcher: (url: string) => fetch(url).then(response => response.json()) }}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SWRConfig>
   )
 }
 
